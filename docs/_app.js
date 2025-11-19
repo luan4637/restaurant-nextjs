279bce24da49151d70ae967de45fb5732cbe752a
Luan Pham <luan4637@gmail.com>
2025-11-19 10:32:22 +0700

    build

--- docs/_app.js
+++
@@ -1,4 +0,0 @@
-var R=require("../chunks/ssr/[turbopack]_runtime.js")("server/pages/_app.js")
-R.c("server/chunks/ssr/[root-of-the-server]__82aa6237._.js")
-R.m(8695)
-module.exports=R.m(8695).exports
--- next.config.js
+++ next.config.js
@@ -1,8 +1,16 @@
 const nextConfig = {
-    devIndicators: false,
+    output: 'export',
+    distDir: 'docs',
+    // devIndicators: false,
     // output: 'standalone',
-    distDir: 'dist/out',
-    // assetPrefix: '/restaurant-nextjs'
+    // distDir: 'dist/out',
+    // assetPrefix: '/restaurant-nextjs',
+    // compiler: {
+    //     styledComponents: {
+    //         ssr: false,
+    //         fileName: true
+    //     }
+    // },
 };
 
 module.exports = nextConfig;
